421e1fee26d5ff5d7e927010a17d41197f7a0631
ludwingra <ludwing.ra@gmail.com>
2020-06-15 00:33:44 -0500

    Primeros pasos, ngif y ngfor

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  name = 'Ludwing';
+  lastname = 'Rivera A.';
+  old = 31;
+}
